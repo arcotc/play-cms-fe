3acaced1820ef1d1d4fdbc0733ee4b7942275555
Mark Hunter <mark.hunter@digital.hmrc.gov.uk>
2017-04-15 19:07:54 +0100

    Add groupTitle

--- app/models/Group.scala
+++ app/models/Group.scala
@@ -18,7 +18,7 @@ package models
 
 import play.api.libs.json.Json
 
-case class Group(name: String)
+case class Group(name: String, title: String)
 
 object Group {
   implicit val reads = Json.reads[Group]
